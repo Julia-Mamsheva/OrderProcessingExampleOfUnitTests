de7160becb088102e1caaac036bad037de53ad48
MKudryash <82795441+MKudryash@users.noreply.github.com>
2025-02-14 14:51:37 +0300

    Add description reamde.md

--- OrderProcessing/OrderResult.cs
+++ OrderProcessing/OrderResult.cs
@@ -6,11 +6,19 @@ using System.Threading.Tasks;
 
 namespace OrderProcessing
 {
+    // Класс OrderResult представляет результат обработки заказа
     public class OrderResult
     {
+        // Свойство, указывающее на успешность обработки заказа
         public bool Success { get; }
+
+        // Свойство, содержащее сообщение о результате обработки заказа
         public string Message { get; }
 
+        /// Конструктор класса, который инициализирует свойства Success и Message
+        /// </summary>
+        /// <param name="success">Указывает, был ли заказ успешно обработан (true) или нет (false)</param>
+        /// <param name="message">Сообщение, содержащее дополнительную информацию о результате обработки заказа</param>
         public OrderResult(bool success, string message)
         {
             Success = success;
